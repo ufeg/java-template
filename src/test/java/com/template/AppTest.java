02b051e724770ecf7247dc2ce05a282fcadaa234
Antoine Warin <antoine.warin@soprasteria.com>
2025-12-03 11:56:15 +0100

    :tada: java template init

---
+++ src/test/java/com/template/AppTest.java
@@ -0,0 +1,19 @@
+package com.template;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest 
+{
+    @Test
+    void doit_retourner_true(){
+        assertTrue(true);
+    }
+
+}
